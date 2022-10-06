dd1de5a50293b6d34de37c930d7f478c24cc93d8
sklajie <sklajie17@gmail.com>
2022-10-06 10:40:30 +0700

    inisiasi resep krebipeti

---
+++ Resep.java
@@ -0,0 +1,23 @@
+/**
+ * Resep
+ */
+public class Resep {
+
+    public static void main(String[] args){
+        System.out.println("Resep Krebipeti : ");
+        System.out.println("");
+        System.out.println("1. Roti");
+        System.out.println("2. Daging");
+        System.out.println("3. Tomat");
+        System.out.println("4. Timun");
+        System.out.println("5. Acar");
+        System.out.println("6. Selada");
+        System.out.println("7. Sosis");
+        System.out.println("8. Mayonaise");
+        System.out.println("9. Saos");
+        System.out.println("10. Telor");
+        System.out.println("11. Keju");
+        System.out.println("12. Tutup Roti");
+    }
+
+}
